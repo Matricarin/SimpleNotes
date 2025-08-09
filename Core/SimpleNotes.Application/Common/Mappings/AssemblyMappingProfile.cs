ed095bacf1f90c8532160a6d8a55785bd2c11557
Matricarin <matricarin@gmail.com>
2025-08-09 18:32:00 +0500

    automapper was been added

---
+++ Core/SimpleNotes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using AutoMapper;
+
+namespace SimpleNotes.Application.Common.Mappings;
+
+public class AssemblyMappingProfile : Profile
+{
+    public AssemblyMappingProfile(Assembly assembly)
+    {
+        ApplyMappingsFromAssembly(assembly);
+    }
+
+    private void ApplyMappingsFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetExportedTypes()
+            .Where(t => t.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapWith<>)));
+
+        foreach (var type in types)
+        {
+            var instance = Activator.CreateInstance(type);
+            if (instance != null)
+            {
+                var methodInfo = type.GetMethod("Mapping");
+                if (methodInfo != null)
+                {
+                    methodInfo.Invoke(instance, new object[] { this });
+                }
+            }
+        }
+    }
+}
--- Core/SimpleNotes.Domain/Note.cs
+++ Core/SimpleNotes.Domain/Note.cs
@@ -2,9 +2,10 @@
 
 public class Note
 {
+    public Guid UserId { get; set; }
     public Guid Id { get; set; }
     public string Title { get; set; } = null!;
     public string? Content { get; set; }
     public DateTime Created { get; set; }
-    public DateTime Modified { get; set; }
+    public DateTime? Modified { get; set; }
 }
