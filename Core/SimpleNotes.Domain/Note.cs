81db6e74fff380db1903a17a42689e62bc478fd6
Matricarin <matricarin@gmail.com>
2025-08-09 16:04:21 +0500

    entity was added

---
+++ Core/SimpleNotes.Domain/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleNotes.Domain;
+
+public class Note
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Content { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Modified { get; set; }
+}
