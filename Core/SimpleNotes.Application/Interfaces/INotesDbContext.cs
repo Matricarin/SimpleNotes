2025048fe0535d04576afe3ab472924ce80ffbed
Matricarin <matricarin@gmail.com>
2025-08-09 18:05:14 +0500

    implement dbcontext

---
+++ Core/SimpleNotes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleNotes.Domain;
+
+namespace SimpleNotes.Application.Interfaces;
+
+public interface INotesDbContext
+{
+    DbSet<Note> Notes { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ Infrastructure/SimpleNotes.Persistence/NotesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleNotes.Application.Interfaces;
+using SimpleNotes.Domain;
+using SimpleNotes.Persistence.EntityTypeConfiguration;
+
+namespace SimpleNotes.Persistence;
+
+public sealed class NotesDbContext : DbContext, INotesDbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new NoteConfiguration());
+        base.OnModelCreating(modelBuilder);
+    }
+}
