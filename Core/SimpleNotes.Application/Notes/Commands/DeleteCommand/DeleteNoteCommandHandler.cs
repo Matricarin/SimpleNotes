4e7d1c436f6ab99fcbed34449744cbe9eeb4d904
Matricarin <matricarin@gmail.com>
2025-08-10 17:40:54 +0500

    delete command was added

---
+++ Core/SimpleNotes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SimpleNotes.Application.Interfaces;
+using SimpleNotes.Application.Notes.Commands.UpdateCommand;
+using SimpleNotes.Domain;
+
+namespace SimpleNotes.Application.Notes.Commands.DeleteCommand;
+
+public class DeleteNoteCommandHandler : IRequestHandler<DeleteNoteCommand>
+{
+    private readonly INotesDbContext _dbContext;
+
+    public DeleteNoteCommandHandler(INotesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
+    {
+        var note = await _dbContext.Notes.FindAsync(request.Id, cancellationToken);
+
+        if (note == null || note.UserId != request.UserId)
+        {
+            throw new NoteNotFoundException(nameof(Note), request.Id);
+        }
+
+        _dbContext.Notes.Remove(note);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
