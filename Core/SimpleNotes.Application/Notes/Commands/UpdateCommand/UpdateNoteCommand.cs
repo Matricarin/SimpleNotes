0fe09e2d8d26d2f339dd6204b81c60d3ecd06de1
Matricarin <matricarin@gmail.com>
2025-08-10 17:22:09 +0500

    update commsnd was implemented

---
+++ Core/SimpleNotes.Application/Notes/Commands/UpdateCommand/UpdateNoteCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace SimpleNotes.Application.Notes.Commands.UpdateCommand;
+
+public class UpdateNoteCommand : IRequest
+{
+    public string Content { get; set; }
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public Guid UserId { get; set; }
+}
---
+++ Core/SimpleNotes.Application/Notes/Commands/UpdateCommand/UpdateNoteCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SimpleNotes.Application.Interfaces;
+using SimpleNotes.Domain;
+
+namespace SimpleNotes.Application.Notes.Commands.UpdateCommand;
+
+public class UpdateNoteCommandHandler : IRequestHandler<UpdateNoteCommand>
+{
+    private INotesDbContext _dbContext;
+
+    public UpdateNoteCommandHandler(INotesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Handle(UpdateNoteCommand request, CancellationToken cancellationToken)
+    {
+        var note = await _dbContext.Notes.FirstOrDefaultAsync(n => n.Id == request.Id, cancellationToken);
+
+        if (note == null || note.UserId != request.UserId)
+        {
+            throw new NoteNotFoundException(nameof(Note), note.Id);
+        }
+
+        note.Content = request.Content;
+        note.Title = request.Title;
+        note.Modified = DateTime.Now;
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
+
+public class NoteNotFoundException : Exception
+{
+    public NoteNotFoundException(string noteName, object key)
+    :base ($"Note [{noteName}] - [{key}] not found.")
+    {
+
+    }
+}
