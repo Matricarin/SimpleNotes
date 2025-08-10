c68b0acc90f904381443be6e39a37d909eff3276
Matricarin <matricarin@gmail.com>
2025-08-10 17:01:56 +0500

    commands was been added

---
+++ Core/SimpleNotes.Application/Notes/Commands/CreateCommand/CreateNoteCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace SimpleNotes.Application.Notes.Commands.CreateCommand;
+
+public class CreateNoteCommand : IRequest<Guid>
+{
+    public Guid UserId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+}
---
+++ Core/SimpleNotes.Application/Notes/Commands/CreateCommand/CreateNoteCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using SimpleNotes.Application.Interfaces;
+using SimpleNotes.Domain;
+
+namespace SimpleNotes.Application.Notes.Commands.CreateCommand;
+
+public class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommand, Guid>
+{
+    private readonly INotesDbContext _dbContext;
+
+    public CreateNoteCommandHandler(INotesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Guid> Handle(CreateNoteCommand request, CancellationToken cancellationToken)
+    {
+        var note = new Note
+        {
+            UserId = request.UserId,
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            Content = request.Content,
+            Created = DateTime.Now,
+            Modified = null
+        };
+
+        await _dbContext.Notes.AddAsync(note, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return note.Id;
+    }
+}
